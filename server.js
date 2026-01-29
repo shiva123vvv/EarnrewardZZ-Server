d2f59021fefe9c23c493ec8e7dee4787d83267cb
shiva123vvv <Shivaneshkumar22@gmail.com>
2026-01-29 06:38:40 +0530

    Fix CORS headers for production

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const SystemSetting = require('./models/SystemSetting');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-firebase-uid', 'x-user-email', 'x-user-name', 'x-referral-code']
+}));
 app.use(express.json());
 
 // Routes
